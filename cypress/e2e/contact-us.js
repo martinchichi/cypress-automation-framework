d323f25fac5b7dcaee3cf4bb0614cfbefe133ff0
martinchichi <martincicinelli@gmail.com>
2024-08-06 17:59:15 -0300

    Basic Cypress Commands

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -1,9 +1,17 @@
+///<reference types = "Cypress" />
+
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-    //cypress code    
+    //cypress code 
+    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+    //cy.get('#contact-us').click({force:true})  
+    cy.get('[name="first_name"]').type("Joe")
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         //cypress code    
         })
 })
+
+
+
