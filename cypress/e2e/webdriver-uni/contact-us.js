d9b2de7835ec59a1881cdb63883789617bbae019
martinchichi <martincicinelli@gmail.com>
2024-08-06 23:11:40 -0300

    Referencing Windows

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,9 +1,12 @@
 ///<reference types = "Cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
-    //cypress code 
+    it.only("Should be able to submit a successful submission via contact us form", () => {
+    //cypress code
     cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+        cy.title().should('include', 'WebDriver | Contact Us')
+        cy.url().should('include', 'contactus')
         //cy.get('#contact-us').click({force: true})
         cy.get('[name="first_name"]').type("Joe");
         cy.get('[name="last_name"]').type("blogs");
