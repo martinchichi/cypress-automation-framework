105cc8782820cb001f7ee378234c1d5b2ac0f041
martinchichi <martincicinelli@gmail.com>
2024-08-06 21:25:18 -0300

    Web Elements & Selectors

---
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -0,0 +1,19 @@
+///<reference types = "Cypress" />
+
+describe("Test Contact Us form via Automation Test Store", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("https://www.automationteststore.com/");
+        //cy.get('.info_links_footer > :nth-child(5) > a').click();
+        cy.get("a[href$='contact']").click();
+        //cy.xpath("//a[contains(@href, 'contact')]").click();
+        cy.get('#ContactUsFrm_first_name').type("Joe");
+        cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
+        //cy.get('.col-md-6 > .btn').click(); 
+        cy.get("button[title='Submit']").click(); 
+    });  
+    
+})
+
+
+
