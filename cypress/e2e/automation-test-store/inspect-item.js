f16fdbbbc29688bcbb080d38a0b9c4c285f2f67d
martinchichi <martincicinelli@gmail.com>
2024-08-15 16:57:04 -0300

    Promises and the Then Command

--- cypress/e2e/automation-test-store/inspect-item.js
+++ cypress/e2e/automation-test-store/inspect-item.js
@@ -8,10 +8,12 @@ describe("Inspect Automation Test Store items using chain of commands", () => {
     
     it.only("Click on the first item using item text", () => {
         cy.visit("https://www.automationteststore.com/");
-        cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click();
+        cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click().then(function(itemHeaderText) {
+            console.log("Selected the following item: " + itemHeaderText.text())
+        })
     });
 
-    it.only("Click on the first item using index", () => {
+    it("Click on the first item using index", () => {
         cy.visit("https://www.automationteststore.com/");
         cy.get('.fixed_wrapper').find('.prdocutname').eq(0).click();
     });
