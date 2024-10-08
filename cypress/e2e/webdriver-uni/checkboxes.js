99c3770e3d36dc70f7f365f2d2de34ed3d041318
martinchichi <martincicinelli@gmail.com>
2024-10-08 19:10:23 -0300

    Handling - Checkboxes

---
+++ cypress/e2e/webdriver-uni/checkboxes.js
@@ -0,0 +1,34 @@
+///<reference types = "cypress" />
+
+describe("Verify checkboxes via webdriveruni", () => {
+    it("Check and validate checkbox", () => {    
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})        
+        
+        cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')        
+        cy.get('#checkboxes > :nth-child(1) > input').as('option-1')        
+        cy.get('@option-1').check().should('be.checked')     
+    })
+
+    it("Uncheck and validate checkbox", () => {    
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})         
+                
+        cy.get(':nth-child(5) > input').as('option-2')        
+        cy.get('@option-2').uncheck().should('not.be.checked')        
+    })
+
+    it("Check multiple checkboxes", () => {    
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})         
+                
+        cy.get("input[type='checkbox']").check(["option-1", "option-2", "option-3", "option-4"]).should('be.checked')
+
+             
+    })
+    
+    
+})
+
+
+
