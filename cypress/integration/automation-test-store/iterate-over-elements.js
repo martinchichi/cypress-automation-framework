eb37ee6a1de368aea6722ce21766f9dd3ca91283
martinchichi-cybel <mcicinelli@cybelesoft.com>
2025-01-17 14:05:44 -0300

    Custom Commands

--- cypress/integration/automation-test-store/iterate-over-elements.js
+++ cypress/integration/automation-test-store/iterate-over-elements.js
@@ -1,24 +1,25 @@
 /// <reference types="cypress" />
 
 describe("Iterate over elements", () => {
-  beforeEach(function() {
+  beforeEach(() => {
     cy.visit("https://automationteststore.com/");
     cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
-  })
+  });
+
   it("Log information of all hair care products", () => {
     cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
-        cy.log("Index: " + index + " : " + $el.text())
+      cy.log("Index: " + index + " : " + $el.text());
     });
   });
   it("Add specific product to basket", () => {
-    cy.selectProduct('Curls to straight Shampoo');
+    cy.selectProduct("Curls to straight Shampoo");
   });
 
   it("Add another specific product to basket", () => {
-    cy.selectProduct('Seaweed Conditioner');
+    cy.selectProduct("Seaweed Conditioner");
   });
 
-  it("Add another specific product to basket 2", () => {
-    cy.selectProduct('Eau Parfumee au The Vert Shampoo');
+  it("Add a third specific product to basket", () => {
+    cy.selectProduct("Eau Parfumee au The Vert Shampoo");
   });
 });
