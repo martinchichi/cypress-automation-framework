30b4ae5cdef5897ad03c5e867b11b763bbcf066a
martinchichi <martincicinelli@gmail.com>
2025-01-19 19:13:15 -0300

    Environment & Global Variables

--- cypress/e2e/webdriver-uni/checkboxes.js
+++ cypress/e2e/webdriver-uni/checkboxes.js
@@ -2,10 +2,7 @@
 
 describe("Verify checkboxes via webdriveruni", () => {
   beforeEach(function () {
-    cy.visit("http://www.webdriveruniversity.com");
-    cy.get("#dropdown-checkboxes-radiobuttons")
-      .invoke("removeAttr", "target")
-      .click({ force: true });
+    cy.navigateTo_WebdriverUni_Checkbox_Page()    
   });
 
   it("Check and validate checkbox", () => {
