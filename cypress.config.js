1e9d8715e2b65533a78536037e3d4b7aaec4840b
martinchichi-cybel <mcicinelli@cybelesoft.com>
2025-01-20 14:17:04 -0300

    Page Object Modelling

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    excludeSpecPattern: "cypress/e2e/other/*.js",
+    baseUrl: "http://www.webdriveruniversity.com",
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
+    env: {
+      first_name: "Sarah",
+      webdriveruni_homepage: "http://www.webdriveruniversity.com"
+    }
+  },
+});
