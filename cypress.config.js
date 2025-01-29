414fd985dce43c766731c72044aa76a211ada94d
martinchichi-cybel <mcicinelli@cybelesoft.com>
2025-01-29 14:30:17 -0300

    Reporting

--- cypress.config.js
+++ cypress.config.js
@@ -16,6 +16,13 @@ module.exports = defineConfig({
     trashAssetsBeforeRuns: true,
     video: false,
     videoCompression: 32,
+    videoUploadOnPasses: false,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json'
+    },
     env: {
       first_name: "Sarah",
       webdriveruni_homepage: "http://www.webdriveruniversity.com"
